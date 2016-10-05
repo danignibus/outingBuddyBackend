f7ee81e2d9a8de3c92aa9b9172c166301389853f
Dani Gnibus <Danielle.N.Gnibus.17@dartmouth.edu>
2016-10-05 11:13:46 -0400

    update readme

--- app/models/outing_model.js
+++ app/models/outing_model.js
@@ -10,3 +10,12 @@ const OutingSchema = new Schema({
 const Outing = mongoose.model('Outing', OutingSchema);
 
 export default Outing;
+
+//structure for users; last contacted interval; something that runs periodically
+//for bots that run on Heroku--either keep awake @ all times or 
+
+//Heroku: if you have an incoming HTTP request to some path, it wakes up
+//Slack: polling client. your Heroku instance polls slack 
+
+//default Heroku plan: schedule something to hit it every hour--Heroku scheduler plugin. 
+//have it listen to /checkup, when it hits URL crank through all entries and figure out whether any is over the time, then clear flags
