3e02308bc03d6b2714cffc07b43bd489687b00fa
Dani Gnibus <Danielle.N.Gnibus.17@dartmouth.edu>
2016-11-02 17:34:55 -0400

    Update algorithm to dynamically add steps to outing until it is full; have at least one step take half the time for outing

--- app/controllers/outing_controller.js
+++ app/controllers/outing_controller.js
@@ -33,47 +33,118 @@ export const getRandomOuting = (req, res) => {
 		});
 };
 
+// export const initiateOuting = (req, res) => {
+// 	var duration = req.query.duration;
+// 	var participants = req.query.participants;
+// 	if (participants == 'UNLIMITED') {
+// 		participants = consts.MAX;
+// 	};
+
+// 	//TODO: will need to change this when an activity doesn't have unlimited participants
+// 	Outing
+// 		.find().where('duration').lte(duration).
+// 		count().
+// 		exec((err, count) => {
+// 			let skip = Math.floor(Math.random() * count);
+// 			Outing.findOne({}).where('duration').lte(duration).
+// 			skip(skip)
+// 			.exec(function(err, obj) {
+// 				getSecondStep(req, res, obj);
+// 			});
+// 		})
+// }
+
+
 export const initiateOuting = (req, res) => {
 	var duration = req.query.duration;
-	var participants = req.query.participants;
-	if (participants == 'UNLIMITED') {
-		participants = consts.MAX;
-	};
+	// 	var participants = req.query.participants;
+	// 	if (participants == 'UNLIMITED') {
+	// 		participants = consts.MAX;
+	// 	};
 
-	//TODO: will need to change this when an activity doesn't have unlimited participants
+	// 	//TODO: will need to change this when an activity doesn't have unlimited participants
+	var halfDuration = Math.ceil(duration/2);
+	var outing = [];
+	var stepIds = [];
+	//find significant outing (i.e. at least half time of outing)
 	Outing
-		.find().where('duration').lte(duration).
+		.find({'duration': halfDuration}).
 		count().
 		exec((err, count) => {
 			let skip = Math.floor(Math.random() * count);
-			Outing.findOne({}).where('duration').lte(duration).
+			Outing.findOne({'duration': halfDuration}).
 			skip(skip)
 			.exec(function(err, obj) {
-				getSecondStep(req, res, obj);
+				// getSecondStep(req, res, obj);
+				outing.push(obj);
+				stepIds.push(obj._id);
+				var remainingDuration = req.query.duration - obj.duration;
+				console.log(remainingDuration);
+				completeOuting(req, res, outing, remainingDuration, stepIds);
 			});
 		})
+	// 	Outing
+// 		.find().where('duration').lte(duration).
+// 		count().
+// 		exec((err, count) => {
+// 			let skip = Math.floor(Math.random() * count);
+// 			Outing.findOne({}).where('duration').lte(duration).
+// 			skip(skip)
+// 			.exec(function(err, obj) {
+// 				getSecondStep(req, res, obj);
+// 			});
+// 		})
+// }
+
 }
 
-export const getSecondStep = (req, res, firstStep) => {
-	var neededDuration = req.query.duration - firstStep.duration;
-	Outing.
-		findOne({'duration': neededDuration}).
-		where('_id').ne(firstStep._id).
-		// where('participants').lte(participants).
-		exec(function(err, obj) {
-			if (obj == null) {
-				Outing.findOne({'duration': req.query.duration}).exec(function(err, obj) {
-					res.json({
-						'detailedSteps': [obj]
-					});
+export const completeOuting = (req, res, outing, remainingDuration, stepIds) => {
+	if (remainingDuration == 0) {
+		console.log('entered base case' + outing);
+		res.json({
+			'detailedSteps': outing
+		})
+	}
+	else if (remainingDuration > 0) {
+		console.log('entered not base case');
+		Outing
+			.find({ '_id': { $nin: stepIds } }).where('duration').lte(remainingDuration).
+			count().
+			exec((err, count) => {
+				let skip = Math.floor(Math.random() * count);
+				Outing.findOne({ '_id': { $nin: stepIds } }).where('duration').lte(remainingDuration).
+				skip(skip)
+				.exec(function(err, obj) {
+					outing.push(obj);
+					stepIds.push(obj._id);
+					remainingDuration = remainingDuration - obj.duration;
+					completeOuting(req, res, outing, remainingDuration, stepIds);
 				});
-			} else {
-				res.json({
-					'detailedSteps': [firstStep, obj]
-				})
-			}
-		});
+			})
+
+	}
 }
+
+// export const getSecondStep = (req, res, firstStep) => {
+// 	var neededDuration = req.query.duration - firstStep.duration;
+// 	Outing.
+// 		findOne({'duration': neededDuration}).
+// 		where('_id').ne(firstStep._id).
+// 		// where('participants').lte(participants).
+// 		exec(function(err, obj) {
+// 			if (obj == null) {
+// 				Outing.findOne({'duration': req.query.duration}).exec(function(err, obj) {
+// 					res.json({
+// 						'detailedSteps': [obj]
+// 					});
+// 				});
+// 			} else {
+// 				res.json({
+// 					'detailedSteps': [firstStep, obj]
+// 				})
+// 			}
+// 		});
+// }
 
 export const getRandomOutingStudy = (callback) => {
 	Outing
